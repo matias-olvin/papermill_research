d1689442f6c7f379dcc0e3933c5d2f5d57c5df78
matias-olvin <matias@olvin.com>
2024-01-19 17:09:49 +0000

    done

--- using_concurrent_futures.py
+++ using_concurrent_futures.py
@@ -1,6 +1,5 @@
 import concurrent.futures
 import papermill as pm
-import time
 
 def execute_notebook(pi, radius):
     parameters = {
@@ -23,16 +22,7 @@ for x in range(1,10):
     task = (pi, radius)
     tasks.append(task)
 
-start_time = time.time()
-
 with concurrent.futures.ThreadPoolExecutor() as executor:  # You can also use ProcessPoolExecutor for multiprocessing
     futures = [executor.submit(execute_notebook, pi, radius) for pi, radius in tasks]
 
 concurrent.futures.wait(futures)
-
-
-end_time = time.time()
-
-elapsed_time = end_time - start_time
-
-print(f"Elapsed time: {elapsed_time} seconds")
--- using_ray.py
+++ using_ray.py
@@ -1,6 +1,10 @@
-from dask import delayed, compute
 import papermill as pm
+import ray
+import time
 
+ray.init()
+
+@ray.remote
 def execute_notebook(pi, radius):
     parameters = {
         "pi": pi,
@@ -16,11 +20,6 @@ def execute_notebook(pi, radius):
         parameters=parameters,
     )
 
-@delayed
-def delayed_execute_notebook(pi, radius):
-    execute_notebook(pi=pi, radius=radius)
+futures = [execute_notebook.remote(3.14, radius) for radius in range(1,10)]
 
-
-tasks = [delayed_execute_notebook(3.14, radius) for radius in range(1,10)]
-
-results = compute(tasks)
+ray.get(futures)
